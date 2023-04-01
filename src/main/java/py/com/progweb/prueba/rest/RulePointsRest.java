f1b26a214b1b6d08609b43e61ec99f489a729fda
Francisco Sanabria <frandepy@fpuna.edu.py>
2023-04-01 19:30:55 -0400

    Agrega GET y POST de regla de puntos

---
+++ src/main/java/py/com/progweb/prueba/rest/RulePointsRest.java
@@ -0,0 +1,29 @@
+package py.com.progweb.prueba.rest;
+
+import py.com.progweb.prueba.ejb.RulePointsDAO;
+import py.com.progweb.prueba.model.RulePoints;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+@Path("rules")
+@Consumes("application/json")
+@Produces("application/json")
+public class RulePointsRest {
+    @Inject
+    RulePointsDAO rulesDAO;
+
+    @GET
+    @Path("/")
+    public Response listar(){
+        return Response.ok(rulesDAO.lista()).build();
+    }
+
+    @POST
+    @Path("/")
+    public Response crear(RulePoints rp){
+        this.rulesDAO.agregar(rp);
+        return Response.ok().build();
+    }
+}
